21872dc6fc5503e68cf8fd14801da4a2d934887b
Travis Perdue <perduetravis@gmail.com>
2017-06-11 09:55:05 -0500

    exercise 3

---
+++ ex3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int age;
+  int height = 72;
+
+  printf("I am %d years old.\n", age);
+  printf("I am %d inces tall.\n", height);
+  return 0;
+}
--- makefile
+++ makefile
@@ -1,4 +1,12 @@
 CFLAGS=-Wall -g
 
+all: ex1 ex3
+
+ex1: ex1.c
+	gcc -o ex1 ex1.c
+
+ex3: ex3.c
+	gcc -o ex3 ex3.c
+
 clean:
-	rm -f ex1
+	rm -f ex1 ex3
