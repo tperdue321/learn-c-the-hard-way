192fa22c641001c9da312ede61342b8c47cf007e
Travis Perdue <perduetravis@gmail.com>
2017-08-27 21:11:48 -0500

    add comments to the file

--- ex16.c
+++ ex16.c
@@ -28,18 +28,28 @@ after:  frees up the memory used up by a Person struct by destroying the Person
 *******************************************************************************/
 void person_destroy(struct Person *person);
 
-
+/*******************************************************************************
+description: prints all the values in a struct of type Person
+params: pass a Person struct to the function. 
+after:  values of the Person struct are printed to the stdout
+*******************************************************************************/
 void person_print(struct Person *person);
 
 int main () {
-  struct Person *joe = person_create("Joe", 8, 52, 80);
-  struct Person *frank = person_create("Frank Blank", 20, 72, 180);
+  struct Person *joe = person_create("Joe", 8, 52, 80); // create a person
+  // create a second person
+  struct Person *frank = person_create("Frank Blank", 20, 72, 180); 
 
+  // print out memory location of person one
   printf("Joe is at memory location: %p\n", joe);
+  // print out the size of the person
   printf("Joe takes up %lu bytes\n", sizeof(struct Person));
+  // print out values of person one
   person_print(joe);
 
+  // print out memory location of person two 
   printf("Frank is at memory location%p:\n", frank);
+  // print out values of person two 
   person_print(frank);
 
   // make everyone 20 years older and print again
@@ -61,9 +71,13 @@ int main () {
 
 
 struct Person *person_create(char *name, int age, int height, int weight) {
+  // create a ptr to a new person struct
   struct Person *person = malloc(sizeof(struct Person));
+  // assert a valid ptr to a person is created 
   assert(person != NULL);
   
+  // dup string to make sure person struct owns it
+  // assign param values to the new person struct
   person->name = strdup(name);
   person->age = age;
   person->height = height;
@@ -73,15 +87,20 @@ struct Person *person_create(char *name, int age, int height, int weight) {
 }
 
 void person_destroy(struct Person * person) {
+  // assert a valid ptr to a person is passed
   assert(person != NULL);
 
+  // free up string memory
   free(person->name);
+  // free up struct memory
   free(person);
 }
 
 void person_print(struct Person *person) {
+  // assert a valid ptr to a person is passed
   assert(person != NULL);
 
+  // print out all the values of the person struct
   printf("Name: %s\n", person->name);
   printf("\tAge: %d\n", person->age);
   printf("\tHeight: %d\n", person->height);
