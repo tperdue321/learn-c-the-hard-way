ac1ef8116a64f87ac763da91ea189a04dc142228
Travis Perdue <perduetravis@gmail.com>
2017-12-17 15:04:31 -0600

    ex19 debug macro

--- dbg.h
+++ dbg.h
@@ -1,30 +1,34 @@
 #ifndef __dbg_h_
 #define __dbg_h_
+// #define NDEBUG 1
 
 
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 
-#ifndef NDEBUG 
+#ifdef NDEBUG 
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", \
-                            __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d:%s: " M "\n", \
+                            __FILE__, __LINE__, __func__, \
+                             ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
 #define log_err(M, ...) fprintf(stderr, \
-        "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, \
-        clean_errno(), ##__VA_ARGS__)
+        "[ERROR] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, \
+        __func__, clean_errno(), ##__VA_ARGS__)
 
 #define log_warn(M, ...) fprintf(stderr, \
-        "[WARN] (%s:%d: errno: %s) " M "\n" \
-        __FILE__, __LINE__, clean_error(), ##__VA_ARGS__)
+        "[WARN] (%s:%d:%s: errno: %s) " M "\n", \
+        __FILE__, __LINE__, __func__, \
+         clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n" \
-        __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d:%s:) " M "\n", \
+        __FILE__, __LINE__, __func__, \
+         ##__VA_ARGS__)
 
 #define check(A, M, ...) if(!(A)) { \
         log_err(M, ##__VA_ARGS__); \
@@ -45,3 +49,5 @@
     errno=0; \
     goto error; \
   }
+
+#endif
